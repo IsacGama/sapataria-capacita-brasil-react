f054a617b6462232b9a52bdf00340bea6ade2b70
Isac Gama <87917139+IsacGama@users.noreply.github.com>
2025-07-11 20:11:40 -0300

    feat: adicionado Botão Comprar, Seletor de Quantidade e Seletor de Tamanho

---
+++ src/Componentes/Buttons/ButtonComprar.js
@@ -0,0 +1,9 @@
+import './ButtonComprar.css'
+
+export default function ButtonComprar() {
+    return (
+        <button className="button-comprar">
+            Comprar
+        </button>
+    );
+}
