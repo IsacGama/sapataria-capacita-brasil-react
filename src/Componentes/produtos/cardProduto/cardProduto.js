8e7a310cf936f1c532ebda10b3502bca027f2a9c
Isac Gama <87917139+IsacGama@users.noreply.github.com>
2025-07-11 17:27:43 -0300

    style: iniciado styles de cardProduto

--- src/Componentes/produtos/cardProduto/cardProduto.js
+++ src/Componentes/produtos/cardProduto/cardProduto.js
@@ -1,27 +1,20 @@
+import './cardProduto.css'
+
 function ProdutoCard({ produto }) {
   const imagemPrincipal = produto.imagens[0];
   const preco = produto.preco;
 
   return (
-    <div>
+    <div className='produto-card'>
       <img
         src={imagemPrincipal}
         alt={produto.nome}
-
+        className="produto-imagem"
       />
-      <h3>{produto.nome}</h3>
-      <p>{produto.descricao || 'Sem descrição'}</p>
-      <p>
-        <strong>À vista:</strong> R$ {preco.aVista.toFixed(2)}
-      </p>
-      <p>
-        <strong>Parcelado:</strong>{' '}
-        {preco.parcelado.map((p, i) => (
-          <span key={i}>
-            {p.parcelas}x de R$ {p.valor.toFixed(2)}{' '}
-          </span>
-        ))}
-      </p>
+      <div className='produto-informacoes'>
+        <h3 className="produto-nome">{produto.nome}</h3>
+        <p className="produto-preco">R$ {preco.aVista.toFixed(2)}</p>
+      </div>
     </div>
   );
 }
