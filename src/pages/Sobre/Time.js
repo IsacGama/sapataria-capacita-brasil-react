952520a5d79daff87bd7142996d87df9dc126e30
Bruno Erick <141072611+bericktk@users.noreply.github.com>
2025-07-15 19:55:43 -0300

    Update Time.js

--- src/pages/Sobre/Time.js
+++ src/pages/Sobre/Time.js
@@ -9,22 +9,22 @@ const equipe = [
   {
     nome: "Beatriz Castro",
     cargo: "Desenvolvedora Full Stack",
-    foto: "https://media.licdn.com/dms/image/v2/D4D03AQE4-xnRdbtZuQ/profile-displayphoto-shrink_800_800/B4DZaH4.ZlG0Ac-/0/1746036583995?e=1758153600&v=beta&t=W_xxx-Zeh32FUk1TVfgIao1mhED7JFjuI25EWrr-VqQ",
+    foto: "https://avatars.githubusercontent.com/u/136024712?v=4",
   },
   {
     nome: "Bruno Erick",
     cargo: "Desenvolvedor Full Stack",
-    foto: "https://media.licdn.com/dms/image/v2/D4D03AQEA6qsZD1nMIA/profile-displayphoto-shrink_800_800/B4DZXdxmyDHwAc-/0/1743182525154?e=1758153600&v=beta&t=TmisEbL5Zl9w7DXncxu_UCTJcexovo2PbA_X6rasPGs",
+    foto: "https://avatars.githubusercontent.com/u/141072611?v=4",
   },
   {
     nome: "Ramiro Dantas",
     cargo: "Desenvolvedor Full Stack",
-    foto: "https://media.licdn.com/dms/image/v2/D4D03AQFaCPlqf3mtCA/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1728528419934?e=1758153600&v=beta&t=2EPoerhb8p43r5Q2WplVeIYvpD_YU_CWN4ZXiJ8nXR4",
+    foto: "https://avatars.githubusercontent.com/u/58824534?v=4",
   },
   {
     nome: "José Cleudiano",
     cargo: "Desenvolvedor Full Stack",
-    foto: "https://media.licdn.com/dms/image/v2/D4D03AQFNiA5-zwZf3A/profile-displayphoto-shrink_800_800/B4DZYYDXboH4Ac-/0/1744160258224?e=1758153600&v=beta&t=tHl-bMh5HoIX4Tqw-ZccMJmshUgZVebbYNN1y85c6sc",
+    foto: "https://avatars.githubusercontent.com/u/202461120?v=4",
   },
   {
     nome: "Adison",
